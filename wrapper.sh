5f972489c45495da282c639fd5930b6217e93d5e
Matthieu Muffato <cortexleglandeur@yahoo.fr>
2018-02-02 21:40:32 +0000

    Added wrapper script to run the python script in an unfriendly environment

---
+++ wrapper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Write here the path to your virtualenv to activate it
+#source $ALTHAPALIGN_VENV/bin/activate
+
+# Write here the location of the directory that contains pybam.py
+#export PYTHONPATH=/path/to/pybam/:$PYTHONPATH
+
+exec "$@"
